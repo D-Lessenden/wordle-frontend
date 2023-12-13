a78936fe6a63cbfa75e6b3747c3d69dd8ca9e856
Daniel Lessenden <dless27@gmail.com>
2023-12-12 22:16:06 -0700

    Now game checks to see if user won or loss after each guess, and if true, transfers back to userProfile

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,6 @@
-// components/Game.js
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './styles/styles.css';
 import QwertyKeyboard from './QwertyKeyboard';
 
@@ -9,11 +9,13 @@ const Game = () => {
   const gameData = location.state.game;
   const userId = location.state.user.id;
   const gameId = gameData.id;
-  console.log(gameData)
+  console.log(gameData.attributes.target_word)
+  console.log(gameData.attributes)
   const [userGuesses, setUserGuesses] = useState(Array.from({ length: 6 }, () => Array(5).fill('')));
   const [activeRow, setActiveRow] = useState(0);
   const [gameState, setGameState] = useState(gameData.game);
   const [submitClicked, setSubmitClicked] = useState(false);
+  const navigate = useNavigate();
 
   const handleInputChange = (rowIndex, cellIndex) => (event) => {
     if (rowIndex === activeRow) {
@@ -43,30 +45,54 @@ const Game = () => {
     if (activeRow < 5) {
       setActiveRow(activeRow + 1);
     }
-
+  
     const apiUrl = `http://127.0.0.1:5000/api/v1/users/${userId}/game/${gameId}`;
-
+  
     try {
-      const response = await fetch(apiUrl, {
+      // Prepare the data to be sent to the server
+      const requestBody = {
+        guesses: formatUserGuesses(), // Include the user guesses
+      };
+  
+      // Make a PATCH request to update the game data
+      const updatedResponse = await fetch(apiUrl, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ guesses: formatUserGuesses() }),
+        body: JSON.stringify(requestBody),
       });
-
-      const updatedGameData = await response.json();
-      console.log(updatedGameData);
-
-      if (response.ok) {
+  
+      // Get the updated game data from the response
+      const updatedGameData = await updatedResponse.json();
+      // console.log('Updated Game Data:', updatedGameData.data.attributes.result);
+      console.log('Result:', updatedGameData);
+  
+      if (updatedResponse.ok) {
         setGameState(updatedGameData);
-        // Do not clear user guesses, let's keep track of all guesses made
+  
+        // Call a separate function to check for a win or loss
+        // console.log(gameData.attributes)
+        checkForWinOrLoss(updatedGameData);
       } else {
-        console.error('Error making guess:', updatedGameData);
+        console.error('Error updating game data:', updatedGameData);
       }
     } catch (error) {
       console.error('Error making guess:', error);
     }
+  };
+  
+  
+  const checkForWinOrLoss = (updatedGameData) => {
+    setGameState((prevState) => {
+      // console.log('check:', updatedGameData);
+      // console.log('check - gb:', updatedGameData.game_board);
+      // console.log('check - history:', updatedGameData.game_history);
+      if (updatedGameData.game_history !== undefined) {
+        navigate(`/user/${userId}`, { state: { user: location.state.user } } );
+      }
+      return prevState; // Return the updated state
+    });
   };
 
   if (!gameData) {
