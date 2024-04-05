852a7183ded09a87a3e4ed4a742471a1c310949b
Daniel Lessenden <dless27@gmail.com>
2024-04-04 22:31:43 -0600

    Create banner

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import UserProfile from './components/UserProfile';
 import Game from './components/Game';
+import Banner from './Banner';
+
 
 function App() {
   return (
     <Router>
       <div>
+        {/* <Banner /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
@@ -25,31 +28,5 @@ function App() {
 }
 
 export default App;
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
